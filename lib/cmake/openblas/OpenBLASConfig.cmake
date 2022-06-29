9ed228b7e61dc8ffe974b717f239122790d903fb
Alexander Stenlund <alexander.stenlund@telia.com>
2022-06-29 16:25:40 +0200

    Removed open blas files

--- lib/cmake/openblas/OpenBLASConfig.cmake
+++
@@ -1,3 +0,0 @@
-SET(OpenBLAS_VERSION "0.3.20")
-SET(OpenBLAS_INCLUDE_DIRS /home/alexanders/Documents/repos/C-Learning/include)
-SET(OpenBLAS_LIBRARIES /home/alexanders/Documents/repos/C-Learning/lib/libopenblas.so)
